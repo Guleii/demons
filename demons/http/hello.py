966d39edd895c976067b81339cb6a3d935f16e1e
Guleii <424307293@qq.com>
2019-03-07 18:00:20 +0800

    http

---
+++ demons/http/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    name = request.args.get('name', 'Flask')  # 获取参数查询参数name的值
+    return '<h1>Hello, %s!</h1>' % name  # 插入到返回值中
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
