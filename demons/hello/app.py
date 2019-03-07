648464fae6d393373e80d205cb1e8ae997a82a68
Guleii <424307293@qq.com>
2019-03-07 13:40:24 +0800

    hello

---
+++ demons/hello/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hi')
+@app.route('/hello')
+def index():
+    return '<h1>Hello Flask!</h1>'
+
+
+@app.route('/greet',
+           defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+@app.cli.command()
+def hello():
+    click.echo('Hello, Human!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
